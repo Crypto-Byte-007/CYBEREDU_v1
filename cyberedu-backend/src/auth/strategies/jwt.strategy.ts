414aa0e3ff804b3879468d48c97af7ab7b6b63e6
Crypto-Byte-007 <arumugapandik1357@gmail.com>
2025-12-24 09:36:15 +0530

    Update jwt.strategy.ts

--- cyberedu-backend/src/auth/strategies/jwt.strategy.ts
+++ cyberedu-backend/src/auth/strategies/jwt.strategy.ts
@@ -1,26 +1,19 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+
 import { AppConfigService } from '../../config/config.service';
-import { JwtPayload } from '../interfaces/tokens.interface';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: AppConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: AppConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.jwt.secret,
+      secretOrKey: config.jwt.accessSecret,
     });
   }
 
-  async validate(payload: JwtPayload) {
-    return {
-      id: payload.sub,
-      email: payload.email,
-      role: payload.role,
-      firstName: payload.firstName,
-      lastName: payload.lastName,
-    };
+  async validate(payload: any) {
+    return payload;
   }
 }
