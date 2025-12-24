0bc158ddd7b4f0c4ae9716b8b6c705e0e40f83c4
Crypto-Byte-007 <arumugapandik1357@gmail.com>
2025-12-24 09:01:21 +0530

    Update password.util.ts

--- cyberedu-backend/src/common/utils/password.util.ts
+++ cyberedu-backend/src/common/utils/password.util.ts
@@ -1,22 +1,31 @@
 import * as bcrypt from 'bcrypt';
 
 export class PasswordUtil {
-  private static readonly SALT_ROUNDS = 10;
+  private static readonly SALT_ROUNDS = 12;
 
   static async hash(password: string): Promise<string> {
-    return await bcrypt.hash(password, this.SALT_ROUNDS);
+    return bcrypt.hash(password, this.SALT_ROUNDS);
   }
 
   static async compare(
     plainText: string,
     hashedPassword: string,
   ): Promise<boolean> {
-    return await bcrypt.compare(plainText, hashedPassword);
+    return bcrypt.compare(plainText, hashedPassword);
   }
 
   static validateStrength(password: string): boolean {
-    // At least 8 characters, 1 uppercase, 1 lowercase, 1 number
-    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/;
+    /**
+     * Requirements:
+     * - 8 to 128 characters
+     * - At least 1 uppercase
+     * - At least 1 lowercase
+     * - At least 1 number
+     * - At least 1 special character
+     */
+    const strongPasswordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^()_+=\-])[A-Za-z\d@$!%*?&#^()_+=\-]{8,128}$/;
+
     return strongPasswordRegex.test(password);
   }
 }
